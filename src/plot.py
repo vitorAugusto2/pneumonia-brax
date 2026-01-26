a7b8394a87ff5406c739c3be35909142dbf24191
Vitor Augusto <131685750+vitorAugusto2@users.noreply.github.com>
2026-01-26 15:13:11 -0300

    Delete src/plot.py

--- src/plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-from sklearn.metrics import (
-    confusion_matrix, ConfusionMatrixDisplay,
-    precision_recall_curve, PrecisionRecallDisplay,
-    roc_curve, RocCurveDisplay
-)
